dc425bd0ad6065367618832dfea544a76dcf276a
a-t-h-i <aonlysmith@gmail.comc>
2023-03-25 15:18:39 +0200

    Created tests

---
+++ tests/test_cases.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest, square as sq
+
+class test_square(unittest.TestCase):
+    
+    def test_is_correct_number(self):
+        self.assertTrue(sq.is_correct_number(5))
+        self.assertFalse(sq.is_correct_number(-1))
+        self.assertTrue(sq.is_correct_number(7))
+        self.assertFalse(sq.is_correct_number(16))
+        self.assertFalse(sq.is_correct_number(0))
+        
+
+    def test_create_empty_square(self):
+        expected = [[0,0,0],
+                    [0,0,0],
+                    [0,0,0]]
+
+        self.assertEqual(expected, sq.create_empty_square(3))
+        self.assertEqual(3, len(sq.create_empty_square(3)))
+
+
+    def test_verify_square(self):
+        self.assertTrue(True)
+
+    def test_magic_square(self):
+        square = sq.create_empty_square(3)
+        magic_square = sq.build_magic_square(3,square)
+        self.assertEqual(1,1)
+
+
